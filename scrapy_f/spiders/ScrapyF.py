47c83473ee6e785dbe3ae032c05d755fa0b8410a
guizhixiao <1002142758@qq.com>
2019-07-01 17:10:26 +0800

    add

--- scrapy_f/spiders/ScrapyF.py
+++ scrapy_f/spiders/ScrapyF.py
@@ -24,26 +24,16 @@ class ScrapyF(scrapy.Spider):
         pass
 
 
-def beautifulSoupparse(self, response):
-    soup = BeautifulSoup(response.body, 'lxml')
-    liLists = soup.select('div.postlist ul#pins li')
-    for liList in liLists:
-        item = ScrapyFItem()
-        soup = BeautifulSoup(str(liList), 'lxml')
-        item["title"] = soup.select('li span a')[0].string
-        item["imgsrc"] = soup.select('li img')[0].attrs['src']
-        item["href"] = soup.select('li>a')[0].attrs['href']
-        # print(title,' <==> ' ,imgsrc)
-        item['referer'] = response.url
-        yield item
+    # def beautifulSoupparse(self, response):
+    #     soup = BeautifulSoup(response.body, 'lxml')
+    #     liLists = soup.select('div.postlist ul#pins li')
+    #     for liList in liLists:
+    #         item = ScrapyFItem()
+    #         soup = BeautifulSoup(str(liList), 'lxml')
+    #         item["title"] = soup.select('li span a')[0].string
+    #         item["imgsrc"] = soup.select('li img')[0].attrs['src']
+    #         item["href"] = soup.select('li>a')[0].attrs['href']
+    #         # print(title,' <==> ' ,imgsrc)
+    #         item['referer'] = response.url
+    #         yield item
 
-
-def test():
-    str1 = '''<li><a href="https://www.mzitu.com/189408" target="_blank"><img alt="貌美似景甜，长腿正妹身姿妖娆温婉可人" class="lazy" data-original="https://i.mei19/06/189408_13a41_236.jpg" height="354" src="https://i.meizitu.net/pfiles/img/lazy.png" width="236"/></a><span><a href="https://www.mzitu.com/189408" target="_blank">貌美似景甜，长腿正妹身姿妖娆温婉可人</a></span><span class="time">2019-06-17</span></li>
-'''
-    href = BeautifulSoup(str1, 'lxml').select('li img')
-    print(href[0].contents)
-
-
-if __name__ == '__main__':
-    test()
